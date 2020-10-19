7c78c64fdfc2eb00ec7af2c753dfd451c24a809c
Philippe Beck <philippe@philippebeck.net>
2020-10-19 20:29:36 +0200

    Add create, update & delete to ResourcesController

--- src/Controller/ResourcesController.php
+++ src/Controller/ResourcesController.php
@@ -21,6 +21,67 @@ class ResourcesController extends MainController
      */
     public function defaultMethod()
     {
-        return $this->render("front/resources.twig");
+        return $this->render("front/resources/resources.twig");
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        if (!empty($this->getPost()->getPostArray())) {
+            $resource = $this->getPost()->getPostArray();
+
+            ModelFactory::getModel("Resources")->createData($resource);
+            $this->getSession()->createAlert("New Resource successfully created !", "green");
+        }
+
+        return $this->render("back/resources/createResource.twig");
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function updateMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        if (!empty($this->getPost()->getPostArray())) {
+            $resource = $this->getPost()->getPostArray();
+
+            ModelFactory::getModel("Resources")->updateData($this->getGet()->getGetVar("id"), $resource);
+            $this->getSession()->createAlert("Successful modification of the selected Resource !", "blue");
+
+            $this->redirect("admin");
+        }
+
+        $resource = ModelFactory::getModel("Method")->readData($this->getGet()->getGetVar("id"));
+
+        return $this->render("back/resources/updateResource.twig", ["resource" => $resource]);
+    }
+
+    public function deleteMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        ModelFactory::getModel("Resources")->deleteData($this->getGet()->getGetVar("id"));
+        $this->getSession()->createAlert("Resource permanently deleted !", "red");
+
+        $this->redirect("admin");
+
     }
 }
