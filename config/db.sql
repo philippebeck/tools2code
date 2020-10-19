ce0d0c873ac35f33e9f1a3a48c1910a9f63ee661
Philippe Beck <philippe@philippebeck.net>
2020-10-19 17:29:38 +0200

    Create DB tables

---
+++ config/db.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS `learn2code`;
+CREATE DATABASE `learn2code` CHARACTER SET utf8;
+
+USE `learn2code`;
+
+CREATE TABLE `Users`
+(
+    `id`    SMALLINT        UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
+    `name`  VARCHAR(50)     NOT NULL,
+    `image` VARCHAR(50)     UNIQUE,
+    `email` VARCHAR(100)    NOT NULL    UNIQUE,
+    `pass`  VARCHAR(100)    NOT NULL,
+    `admin` TINYINT(1)      NOT NULL,
+)
+    ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Courses`
+(
+  `id`      SMALLINT        UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
+  `name`    VARCHAR(20)     NOT NULL    UNIQUE,
+  `content` TEXT
+)
+ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Resources`
+(
+  `id`      SMALLINT        UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
+  `name`    VARCHAR(20)     NOT NULL    UNIQUE,
+  `content` TEXT
+)
+ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Training`
+(
+  `id`      SMALLINT        UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
+  `name`    VARCHAR(20)     NOT NULL    UNIQUE,
+  `content` TEXT
+)
+ENGINE=INNODB DEFAULT CHARSET=utf8;
