4b7d830b545df9757d96f311110a57a0ea44e312
Philippe Beck <philippe@philippebeck.net>
2021-03-02 12:36:01 +0100

    Remove useless methods to UsersController

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -19,22 +19,6 @@ class UsersController extends MainController
      */
     private $user = [];
 
-    private function checkLogin()
-    {
-        $user = ModelFactory::getModel("Users")->readData($this->user["email"], "email");
-
-        if (!password_verify($this->user["pass"], $user["pass"])) {
-            $this->getSession()->createAlert("Failed authentication !", "black");
-
-            $this->redirect("users");
-        }
-
-        $this->getSession()->createSession($user);
-        $this->getSession()->createAlert("Successful authentication, welcome " . $user["name"] . " !", "purple");
-
-        $this->redirect("admin");
-    }
-
     /**
      * @return string
      * @throws LoaderError
@@ -43,29 +27,7 @@ class UsersController extends MainController
      */
     public function defaultMethod()
     {
-        if (!empty($this->getPost()->getPostArray())) {
-            $this->user = $this->getPost()->getPostArray();
-
-            if (isset($this->user["g-recaptcha-response"]) && !empty($this->user["g-recaptcha-response"])) {
-
-                if ($this->getSecurity()->checkRecaptcha($this->user["g-recaptcha-response"])) {
-                    $this->checkLogin();
-                }
-            }
-
-            $this->getSession()->createAlert("Check the reCAPTCHA !", "red");
-
-            $this->redirect("users");
-        }
-
         return $this->render("front/login.twig");
-    }
-
-    public function logoutMethod()
-    {
-        $this->getSession()->destroySession();
-
-        $this->redirect("home");
     }
 
     private function setUserData()
